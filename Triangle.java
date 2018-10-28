89cb42811fdc4d106f235efa2fa51d0e41852430
nicholasbrean91 <31775539+nicholasbrean91@users.noreply.github.com>
2018-10-28 18:01:01 -0400

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -24,23 +24,19 @@ public class Triangle {
 		if (type.equalsIgnoreCase("SSS")){
 			Secondary obj = new Secondary(type, info1, info2, info3);
 			
-			obj.isTriangleValid();
+				obj.isTriangleValid();
 			
-			//Check Side Lengths
-			//if (Side1 < Side2 + Side3 && Side2 < Side1 + Side3 && Side3 < Side1 + Side2){
-				//if (Side1 <= 0||Side2 <= 0||Side3 <= 0){
-					//System.out.println("In our universe we cannot have negative lengths, please enter in a positive side length.");
-					//System.exit(0);
-				//}
 				//Find Angels
-				double angelA = obj.getAngleA(Side1, Side2, Side3);
-				double angelB = obj.getAngleB(Side1, Side2, Side3);
-				double angelC = obj.getAngleC(Side1, Side2, Side3);
+				double angelA = obj.getAngleA();
+				double angelB = obj.getAngleB();
+				double angelC = obj.getAngleC();
 				
-				double triangleType = obj.triangleTypeCalc(Side1, Side2, Side3);
+
+				double triangleType = Secondary.triangleTypeCalc();
+
 				
 				if (triangleType == 1){
-					if(angelA < 90 && angelB < 90 && angelC <90){
+					if(angelA < 90 && angelB < 90 && angelC < 90){
 						System.out.println("This triangle is an Acute Isosceles triangle");
 					}else{
 						System.out.println("This triangle is an Obtuse Isosceles triangle");
@@ -56,17 +52,27 @@ public class Triangle {
 						System.out.println("This triangle is an Obtuse Squalene triangle");
 				}
 				//Perimeter
-				double totalperm = obj.perimeterCalc(Side1, Side2, Side3);
+				double totalperm = obj.perimeterCalc();
 				System.out.println("The total Perimeter is " + totalperm + "cm" );
 				
 				//Area
-				double semi = obj.semiPerimeter(Side1, Side2, Side3);
+				double semi = obj.semiPerimeter();
 				double area = obj.heronFormula(semi);
+				
 				System.out.println("The area of the triangle is " + area + "cm^2");
 				
 			}else{
 				System.out.println("Enter a correct Triangle.");	
 			}	
+				
+		}else if(type.equalsIgnoreCase("SAS")){
+			Secondary obj = new Secondary(type, info1, info2, info3);
+			double sideC = obj.getSideC();
+			obj.isTriangleValid();
+			
+			//System.out.println(sideC);
+			
+			
 		}
 	 }
   }
