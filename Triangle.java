3cc0a2e17e9dc0ece6ed0bb7c5f94d0b146a47d8
nicholasbrean91 <31775539+nicholasbrean91@users.noreply.github.com>
2018-10-30 22:39:26 -0400

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -13,13 +13,17 @@ public class Triangle {
 		int info2 = 0;
 		int info3 = 0;
 		
-		System.out.println("Choose One of the below options.\n \n 1) SSS for 3 Sides.\n 2) SAS for 1 Side then 1 Angel and 1 Side (angel is in between two sides).\n 3) ASA for 1 Angle 1 Side and 1 Angle.\n 4) SSA for 1 Side 1 Side and 1 Angle (not an angle between the sides).\n 5) AAS 2 Angles and 1 Side.\n");
+		System.out.println("Choose One of the below options.\n \n 1) SSS for 3 Sides.\n "
+							+ "2) SAS for 1 Side then 1 Angel and 1 Side (angel is in between two sides).\n "
+							+ "3) ASA for 1 Angle 1 Side and 1 Angle.\n "
+							+ "4) SSA for 1 Side 1 Side and 1 Angle (not an angle between the sides).\n "
+							+ "5) AAS 2 Angles and 1 Side.\n");
+		
 		System.out.print("Enter one of the abberivations for selection. (Ex.'SSS' for Side Side Side): ");
 		type = userInput.nextLine(); //What you're solving for
 
-		
 		if (type.equalsIgnoreCase("SSS")){
-			System.out.println("\n'SSS' is when you have 3 sides of a Triangle.\n"); //Needed to keep these Print statemnts inside each function becuase each is a little different
+			System.out.println("\n'SSS' is when you have 3 sides of a Triangle.\n"); 
 			System.out.print("Enter in side 1 according to this preset " + type + " : ");
 			info1 = userInput.nextInt(); 
 			System.out.print("Enter in side 2 according to this preset " + type + " : ");
@@ -38,6 +42,11 @@ public class Triangle {
 			//Area
 			obj.semiPerimeter();
 			obj.heronFormula();
+			
+			//Height
+			obj.heightAngle3();
+			obj.heightAngle2();
+			obj.heightAngle1();
 				
 		}else if(type.equalsIgnoreCase("SAS")){
 			System.out.println("\n'SAS' is when we know two sides and the angle between them.\n");
@@ -60,6 +69,11 @@ public class Triangle {
 			//Area
 			obj.semiPerimeter();
 			obj.heronFormula();
+			
+			//Height
+			obj.heightAngle3();
+			obj.heightAngle2();
+			obj.heightAngle1();
 
 		}else if(type.equalsIgnoreCase("ASA")){
 			System.out.println("\nASA Triangle is when we know two angles and a side between the angles.\n ");
@@ -85,6 +99,11 @@ public class Triangle {
 			obj.semiPerimeter();
 			obj.heronFormula();
 			
+			//Height
+			obj.heightAngle3();
+			obj.heightAngle2();
+			obj.heightAngle1();
+			
 		}else if (type.equalsIgnoreCase("AAS")) {
 			System.out.println("\n"  + 	"\"AAS\" is when we know two angles and one side (which is not between the angles).\n ");
 			System.out.print("Enter in angle1 according to this preset " + type + " : ");
@@ -109,6 +128,11 @@ public class Triangle {
 			obj.semiPerimeter();
 			obj.heronFormula();
 			
+			//Height
+			obj.heightAngle3();
+			obj.heightAngle2();
+			obj.heightAngle1();
+			
 		}else if(type.equalsIgnoreCase("SSA")){
 			System.out.println("\n'SSA' is when we know two sides and an angle that is not the angle between the sides.\n");
 			System.out.print("Enter in side2 according to this preset " + type + " : ");
@@ -132,6 +156,11 @@ public class Triangle {
 			//Area
 			obj.semiPerimeter();
 			obj.heronFormula();
+			
+			//Height
+			obj.heightAngle3();
+			obj.heightAngle2();
+			obj.heightAngle1();
 			
 		}else{
 			System.out.println("Please Enter one of the presets above, Note it is case sensitive");
